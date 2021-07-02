5815b367e92b675b982097071c8e73d66b29b958
Shiva Kumar <koduri.sneu@gmail.com>
2021-07-02 14:24:29 -0400

    commit on master

---
+++ Backend/app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //  url: "mongodb://localhost:27017/nu-events_db" //mrigesh
+    url:"mongodb+srv://mrigDbUser:mrigDbUser@cluster0.obnhx.mongodb.net/nu-events_db?retryWrites=true&w=majority"
+  };
